cba9bb906051da6284116a8d47b660246901cada
Lucas Oriental dos Santos <lucasoriental16@gmail.com>
2024-03-23 15:06:57 -0300

    main
    Formatting some files.

--- src/components/global/footer/footer.jsx
+++ src/components/global/footer/footer.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    return (
-        <footer>
-            <h1>Footer</h1>
-        </footer>
-    )}
+  return (
+    <footer>
+      <h1>Footer</h1>
+    </footer>
+  );
+};
 
 export default Footer;
