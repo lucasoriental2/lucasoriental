dc8f75e565ef6c430646b93c0ab344fcd54c14f5
Lucas Oriental dos Santos <lucasoriental16@gmail.com>
2024-04-02 12:28:29 -0300

    main
    Job Experiences Page

---
+++ src/components/sections/job-experiences-page/common/jobExperience.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import imageTemp from "../../../../assets/pictures/img_empty.png"
+
+export default function JobExperiences (props) {
+
+    return (
+      <div className="block-of-jobs">
+        {props.data.map((job, index) => {
+          return (
+            <div className="job-entire-item" key={index}>
+              <img alt="img" src={imageTemp} className="job-image" />
+              <div className="job-entire-item-paragraph">
+                <p className="job-title">{job.jobTitle}</p>
+                <p className="job-company-name">{job.companyName}</p>
+                <p className="job-time">{job.time}</p>
+                <p className="job-responsibilities">{job.responsibilities}</p>
+                <a href="#" className="job-button-link">
+                  <button className="job-button">more details</button>
+                </a>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
+}
--- src/components/sections/job-experiences-page/jobExperiences.jsx
+++ src/components/sections/job-experiences-page/jobExperiences.jsx
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import JobExperience from "../job-experiences-page/common/jobExperience"
 
 const JobExperiencesPage = () => {
+    const [isLoading, setIsLoading] = useState(true)
+    const [jobExperiences, setJobExperiences] = useState(null)
+
+    useEffect( () => {
+        const fetchData = async () => {
+          try {
+            const response = await fetch(
+              "http://localhost:5000/jobExperiences"
+            );
+            setJobExperiences(await response.json());
+            setIsLoading(false);
+          } catch (err) {
+            console.error(
+              "Something error happend during fetch on the Job Experiences page, please try refreshing the site..."
+            );
+          }
+        }
+        fetchData();
+    }, [])
+
     return (
-        <><h1>Job Experiences Page</h1></>
-    )
+      <div id="job-experiences-container">
+        <div>
+          <p className="title-general">JOB EXPERIENCES</p>
+        </div>
+        {isLoading === true ? (
+          <p
+          style={{
+            textAlign: "center",
+            fontSize: "25px",
+            color: "#FFF",
+            margin: "90px 0px",
+            fontWeight: "500",
+          }}
+        >
+          The data is loading, please wait a moment....
+        </p>
+        ) : (
+          <JobExperience data={jobExperiences} />
+        )}
+      </div>
+    );
 }
 
 export default JobExperiencesPage;
