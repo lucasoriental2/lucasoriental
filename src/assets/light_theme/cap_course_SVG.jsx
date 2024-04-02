91f3093a51de7bac720b0dea9f09fa644e091f24
Lucas Oriental dos Santos <lucasoriental16@gmail.com>
2024-04-02 12:29:50 -0300

    main
    fixing the svg file in the code

--- src/assets/light_theme/cap_course_SVG.jsx
+++ src/assets/light_theme/cap_course_SVG.jsx
@@ -2,7 +2,7 @@ export default function CapCourseLightSVG() {
   return (
     <svg
       width="20"
-      height="=18"
+      height="18"
       viewBox="0 0 10 11"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
