61b81e2c5fd2dc02eae2ffac5ceba4600e3d78ac
Lucas Oriental dos Santos <lucasoriental16@gmail.com>
2024-04-02 12:34:00 -0300

    main
    Some temp modifications under scss(s) files!
    It will be updated to the right settings as soon as possible.

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,26 @@
-import React from 'react'
-import './App.scss'
-import Header from '../src/components/global/header/header'
-import Footer from '../src/components/global/footer/footer'
-import MainPage from './components/sections/home-page/home'
-import MyProjects from './components/sections/my-projects-page/myProjects'
-import EducationAndSkills from './components/sections/education-and-skills-page/educationAndSkills'
-import JobExperiences from './components/sections/job-experiences-page/jobExperiences'
-import AboutMe from './components/sections/about-me-page/aboutMe'
-import ContactMe from './components/sections/contact-me-page/contactMe'
-import HaveAFun from './components/sections/have-a-fun-page/haveAFun'
+import React from "react";
+import Header from "../src/components/global/header/header";
+import "./App.scss";
+import EducationAndSkills from "./components/sections/education-and-skills-page/educationAndSkills";
+import MainPage from "./components/sections/home-page/home";
+import MyProjects from "./components/sections/my-projects-page/myProjects";
+import JobExperiences from "./components/sections/job-experiences-page/jobExperiences";
+import AboutMe from "./components/sections/about-me-page/aboutMe";
 
 function App() {
   return (
     <>
       <Header />
-      {/* <MainPage />
+      <MainPage />
       <MyProjects />
       <EducationAndSkills />
       <JobExperiences />
       <AboutMe />
-      <ContactMe />
+      {/*<ContactMe />
       <HaveAFun />
       <Footer /> */}
     </>
   );
 }
 
-export default App
+export default App;
