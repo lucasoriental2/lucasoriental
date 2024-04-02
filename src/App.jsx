4109255a3cf018bfdb1a71dc8290d1de9dc1ed16
Lucas Oriental dos Santos <lucasoriental16@gmail.com>
2024-04-02 15:21:55 -0300

    main
    Starting to setup the contact me form page!
    it will be updated in the future.

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import MainPage from "./components/sections/home-page/home";
 import MyProjects from "./components/sections/my-projects-page/myProjects";
 import JobExperiences from "./components/sections/job-experiences-page/jobExperiences";
 import AboutMe from "./components/sections/about-me-page/aboutMe";
+import ContactMe from "./components/sections/contact-me-page/contactMe";
 
 function App() {
   return (
@@ -16,8 +17,8 @@ function App() {
       <EducationAndSkills />
       <JobExperiences />
       <AboutMe />
-      {/*<ContactMe />
-      <HaveAFun />
+      <ContactMe />
+      {/*<HaveAFun />
       <Footer /> */}
     </>
   );
--- src/components/sections/contact-me-page/contactMe.jsx
+++ src/components/sections/contact-me-page/contactMe.jsx
@@ -1,9 +1,128 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const contactMePage = () => {
+
+    const [nameErr, setName] = useState("")
+    const [emailErr, setEmail] = useState("")
+
+    const onChange = (e) => {
+      if (e.target.name === "nameUser") {
+        setName(e.target.value === "" ? "empty is not allowed" : "");
+      }
+      if (e.target.name === "email"){
+        setEmail(e.target.value === "" ? "empty is not allowed" : "");
+      }
+    }
+
+    const handleSubmit = async (e) => {
+        try {
+          e.preventDefault();
+        } catch (err) {
+          console.error(err);
+        }
+    }
+
     return (
-        <><h1>Contact Me Page</h1></>
-    )
+      <div id="contact-me-container">
+        <p className="title-general">CONTACT ME</p>
+        <div className="contact-me-form-social">
+          <div className="contact-me-left-side">
+            <div>
+              <div>
+                <p>get in touch</p>
+                <canvas></canvas>
+              </div>
+              <div>
+                <div>
+                  <a href="#">
+                    <p>Rio de Janeiro (RJ) - Brazil</p>
+                    <>SVG</>
+                  </a>
+                </div>
+                <div>
+                  <a href="#">
+                    <p>lucasoriental@gmail.com</p>
+                    <>SVG</>
+                  </a>
+                </div>
+                <div>
+                  <a href="#">
+                    <p>+55 (21) 99525-3580</p>
+                    <>SVG</>
+                  </a>
+                </div>
+                <div>
+                  <a href="#">
+                    <p>@lucas_oriental</p>
+                    <>SVG</>
+                  </a>
+                </div>
+                <div>
+                  <a href="#">
+                    <p>/lucasoriental</p>
+                    <>SVG</>
+                  </a>
+                </div>
+                <div>
+                  <a href="#">
+                    <p>Lucas Oriental dos Santos</p>
+                    <>SVG</>
+                  </a>
+                </div>
+                <div>
+                  <a href="#">
+                    <p>/lucasoriental</p>
+                    <>SVG</>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="contact-me-right-side">
+            <p className="title-form-right-side">SEND ME A MESSAGE</p>
+            <form id="form-contact-me">
+              <label>
+                <input
+                  required
+                  type="text"
+                  id="name-field"
+                  name="nameUser"
+                  placeholder="Name"
+                  onChange={(e) => onChange(e)}
+                />
+                {nameErr.length > 0 && <p>{nameErr}</p>}
+              </label>
+              <label>
+                <input
+                  invalid={emailErr.length > 0}
+                  required
+                  type="email"
+                  id="email-field"
+                  name="email"
+                  placeholder="E-mail Address"
+                  onChange={(e) => onChange(e)}
+                />
+                {emailErr.length > 0 && <p>{emailErr}</p>}
+              </label>
+              <label>
+                <input
+                  required
+                  type="text"
+                  id="textBox-field"
+                  name="message"
+                  placeholder="Type anything you want..."
+                  maxLength="500"
+                  onChange={(e) => onChange(e)}
+                />
+                {emailErr.length > 0 && <p>{emailErr}</p>}
+              </label>
+
+              <input type="submit" className="button-right-side-form" value="Submit" />
+            </form>
+          </div>
+        </div>
+      </div>
+    );
 }
 
 export default contactMePage;
