ec22d6923b2304ac4a6ab0f40c8caf2bac6d2db5
Lucas Oriental dos Santos <lucasoriental16@gmail.com>
2024-04-02 12:30:59 -0300

    main
    About Me page

--- src/components/sections/about-me-page/aboutMe.jsx
+++ src/components/sections/about-me-page/aboutMe.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import image from "../../../assets/pictures/project_img.png"
 
 const aboutMePage = () => {
-    return (
-        <><h1>About Me Page</h1></>
-    )
+  return (
+    <div id="about-me-container">
+      <p className="title-general" style={{ color: "#E00036" }}>
+        ABOUT ME
+      </p>
+      <div className="about-me-container-items">
+        <div className="img-and-text">
+          <img alt="imagem" src={image} className="about-me-image"/>
+          <p className="about-me-text-aside"> texto do ladotexto do ladotexto do ladotexto do ladotexto do ladotexto do ladotexto do ladotexto do ladotexto do ladotexto do ladotexto do ladotexto do ladotexto do ladotexto do ladotexto do ladotexto do ladotexto do ladotexto do ladotexto do ladotexto do ladotexto do ladotexto do ladotexto do ladotexto do ladotexto do ladotexto do ladotexto do ladotexto do ladotexto do lado</p>
+        </div>
+        <p className="about-me-text">texto abaixo texto abaixo texto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixo</p>
+        <p className="about-me-text">texto abaixo texto abaixo texto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixotexto abaixo</p>
+      </div>
+    </div>
+  );
 }
 
 export default aboutMePage;
