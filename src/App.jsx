6674631ece07b91aa56c78824a9b89dc8f97d2a6
Lucas Oriental dos Santos <lucasoriental16@gmail.com>
2024-04-16 11:49:19 -0300

    main
    The footer will be developed as soon

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Header from "../src/components/global/header/header";
 import "./App.scss";
+import 'animate.css';
 import EducationAndSkills from "./components/sections/education-and-skills-page/educationAndSkills";
 import MainPage from "./components/sections/home-page/home";
 import MyProjects from "./components/sections/my-projects-page/myProjects";
 import JobExperiences from "./components/sections/job-experiences-page/jobExperiences";
 import AboutMe from "./components/sections/about-me-page/aboutMe";
 import ContactMe from "./components/sections/contact-me-page/contactMe";
+import Footer from './components/global/footer/footer'
+
+
 
 function App() {
   return (
@@ -18,8 +22,7 @@ function App() {
       <JobExperiences />
       <AboutMe />
       <ContactMe />
-      {/*<HaveAFun />
-      <Footer /> */}
+      <Footer />
     </>
   );
 }
