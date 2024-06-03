0d00e926cbe21272b1a4f89d328e6a1f65068d82
Lucas Oriental dos Santos <lucasoriental16@gmail.com>
2024-06-03 01:11:20 -0300

    SCRUM-MASTER
    Removing some useless implementation of Contact Me Section

--- src/components/sections/contact-me-page/contactMe.jsx
+++ src/components/sections/contact-me-page/contactMe.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from "react";
 import BehanceSVG from "../../../assets/light_theme/Behance";
 import GitHubSVG from "../../../assets/light_theme/GitHub";
 import InstagramSVG from "../../../assets/light_theme/Instagram";
@@ -8,177 +8,99 @@ import MailSVG from "../../../assets/light_theme/mail";
 import NumberSVG from "../../../assets/light_theme/wpp_or_call";
 
 const contactMePage = () => {
-
-    const [nameErr, setNameErr] = useState("")
-    const [emailErr, setEmailErr] = useState("")
-    const [textBoxErr, setTextBoxErr] = useState("")
-
-    const onChange = (e) => {
-      if (e.target.name === "nameUser") {
-        setNameErr(e.target.value === "" ? "This field is required" : "");
-      }
-      if (e.target.name === "email"){
-        setEmailErr(e.target.value === "" ? "This field is also required" : "");
-      }
-      if (e.target.name === "message"){
-        setTextBoxErr(e.target.value === "" ? "This field too :D" : "");
-      }
-    }
-
-    const handleSubmit = async (e) => {
-        try {
-          e.preventDefault();
-        } catch (err) {
-          console.error(err);
-        }
-    }
-
-    return (
-      <div id="contact-me-container">
-        <div className="container-general">
-          <p className="title-general">CONTACT ME</p>
-          <div className="contact-me-form-social">
-            <div className="contact-me-left-side">
-              <div>
-                <div className="left-side-title-and-canvas">
-                  <p className="left-side-form-title">Get in Touch!</p>
-                  <canvas className="left-side-form-above-title" />
+  return (
+    <div id="contact-me-container">
+      <div className="container-general">
+        <p className="title-general">CONTACT ME</p>
+        <div className="contact-me-form-social">
+          <div className="contact-me-left-side">
+            <div>
+              <div className="left-side-title-and-canvas">
+                <p className="left-side-form-title">Get in Touch!</p>
+                <canvas className="left-side-form-above-title" />
+              </div>
+              <div className="left-side-all-items">
+                <div className="left-side-item">
+                  <a
+                    href="https://maps.app.goo.gl/WRGVPhNip7Dmu8Zg7"
+                    target="_blank"
+                    className="left-side-item-a"
+                  >
+                    <p className="social-media-name">
+                      Rio de Janeiro (RJ) - Brazil
+                    </p>
+                    <LocationSVG width={40} height={41} color={"#E00036"} />
+                  </a>
                 </div>
-                <div className="left-side-all-items">
-                  <div className="left-side-item">
-                    <a
-                      href="https://maps.app.goo.gl/WRGVPhNip7Dmu8Zg7"
-                      target="_blank"
-                      className="left-side-item-a"
-                    >
-                      <p className="social-media-name">
-                        Rio de Janeiro (RJ) - Brazil
-                      </p>
-                      <LocationSVG width={40} height={41} color={"#E00036"} />
-                    </a>
-                  </div>
-                  <div className="left-side-item">
-                    <a
-                      href="mailto:lucasoriental@gmail.com"
-                      target="_blank"
-                      className="left-side-item-a"
-                    >
-                      <p className="social-media-name">
-                        lucasoriental@gmail.com
-                      </p>
-                      <MailSVG width={40} height={37} color={"#E00036"} />
-                    </a>
-                  </div>
-                  <div className="left-side-item">
-                    <a
-                      href="https://web.whatsapp.com/send?phone=5521995253580"
-                      target="_blank"
-                      className="left-side-item-a"
-                    >
-                      <p className="social-media-name">+55 (21) 99525-3580</p>
-                      <NumberSVG width={39} height={42} color={"#E00036"} />
-                    </a>
-                  </div>
-                  <div className="left-side-item">
-                    <a
-                      href="https://instagram.com/lucasorientaldev"
-                      target="_blank"
-                      className="left-side-item-a"
-                    >
-                      <p className="social-media-name">@lucasorientaldev</p>
-                      <InstagramSVG width={40} height={40} color={"#E00036"} />
-                    </a>
-                  </div>
-                  <div className="left-side-item">
-                    <a
-                      href="https://github.com/lucasoriental"
-                      className="left-side-item-a"
-                      target="_blank"
-                    >
-                      <p className="social-media-name">/lucasoriental</p>
-                      <GitHubSVG width={40} height={40} color={"#E00036"} />
-                    </a>
-                  </div>
-                  <div className="left-side-item">
-                    <a
-                      href="https://linkedin.com/in/lucas-oriental-dos-santos/"
-                      target="_blank"
-                      className="left-side-item-a"
-                    >
-                      <p className="social-media-name">
-                        Lucas Oriental dos Santos
-                      </p>
-                      <LinkedInSVG width={40} height={40} color={"#E00036"} />
-                    </a>
-                  </div>
-                  <div className="left-side-item">
-                    <a
-                      href="https://behance.net/lucasoriental"
-                      target="_blank"
-                      className="left-side-item-a"
-                    >
-                      <p className="social-media-name">/lucasoriental</p>
-                      <BehanceSVG width={40} height={40} color={"#E00036"} />
-                    </a>
-                  </div>
+                <div className="left-side-item">
+                  <a
+                    href="mailto:lucasoriental@gmail.com"
+                    target="_blank"
+                    className="left-side-item-a"
+                  >
+                    <p className="social-media-name">lucasoriental@gmail.com</p>
+                    <MailSVG width={40} height={37} color={"#E00036"} />
+                  </a>
+                </div>
+                <div className="left-side-item">
+                  <a
+                    href="https://web.whatsapp.com/send?phone=5521995253580"
+                    target="_blank"
+                    className="left-side-item-a"
+                  >
+                    <p className="social-media-name">+55 (21) 99525-3580</p>
+                    <NumberSVG width={39} height={42} color={"#E00036"} />
+                  </a>
+                </div>
+                <div className="left-side-item">
+                  <a
+                    href="https://instagram.com/lucasorientaldev"
+                    target="_blank"
+                    className="left-side-item-a"
+                  >
+                    <p className="social-media-name">@lucasorientaldev</p>
+                    <InstagramSVG width={40} height={40} color={"#E00036"} />
+                  </a>
+                </div>
+                <div className="left-side-item">
+                  <a
+                    href="https://github.com/lucasoriental"
+                    className="left-side-item-a"
+                    target="_blank"
+                  >
+                    <p className="social-media-name">/lucasoriental</p>
+                    <GitHubSVG width={40} height={40} color={"#E00036"} />
+                  </a>
+                </div>
+                <div className="left-side-item">
+                  <a
+                    href="https://linkedin.com/in/lucas-oriental-dos-santos/"
+                    target="_blank"
+                    className="left-side-item-a"
+                  >
+                    <p className="social-media-name">
+                      Lucas Oriental dos Santos
+                    </p>
+                    <LinkedInSVG width={40} height={40} color={"#E00036"} />
+                  </a>
+                </div>
+                <div className="left-side-item">
+                  <a
+                    href="https://behance.net/lucasoriental"
+                    target="_blank"
+                    className="left-side-item-a"
+                  >
+                    <p className="social-media-name">/lucasoriental</p>
+                    <BehanceSVG width={40} height={40} color={"#E00036"} />
+                  </a>
                 </div>
               </div>
             </div>
-            {/* <div className="contact-me-right-side">
-              <p className="title-form-right-side">SEND ME A MESSAGE</p>
-              <form id="form-contact-me">
-                <label>
-                  <input
-                    required
-                    type="text"
-                    id="name-field"
-                    name="nameUser"
-                    placeholder="Name"
-                    onChange={(e) => onChange(e)}
-                  />
-                  {nameErr.length > 0 && (
-                    <p className="form-field-error-msg">{nameErr}</p>
-                  )}
-                </label>
-                <label>
-                  <input
-                    required
-                    type="email"
-                    id="email-field"
-                    name="email"
-                    placeholder="E-mail Address"
-                    onChange={(e) => onChange(e)}
-                  />
-                  {emailErr.length > 0 && (
-                    <p className="form-field-error-msg">{emailErr}</p>
-                  )}
-                </label>
-                <label>
-                  <input
-                    required
-                    type="text"
-                    id="textBox-field"
-                    name="message"
-                    placeholder="Type anything you want..."
-                    maxLength="500"
-                    onChange={(e) => onChange(e)}
-                  />
-                  {textBoxErr.length > 0 && (
-                    <p className="form-field-error-msg">{textBoxErr}</p>
-                  )}
-                </label>
-
-                <input
-                  type="submit"
-                  className="button-right-side-form"
-                  value="Submit"
-                />
-              </form>
-            </div> */}
           </div>
         </div>
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default contactMePage;
