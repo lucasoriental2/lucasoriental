db99fedfbda0b2dc84cd9459012367ba58d213e7
Lucas Oriental dos Santos <lucasoriental16@gmail.com>
2024-06-11 23:51:05 -0300

    fixing the modal

--- src/components/sections/job-experiences-page/common/jobExperience.jsx
+++ src/components/sections/job-experiences-page/common/jobExperience.jsx
@@ -10,27 +10,34 @@ export default function JobExperiences(props) {
     <div className="block-of-jobs">
       {props.data.map((job, index) => {
         return (
-          <div className="job-entire-item" key={job.id} data-aos="fade-right" data-aos-anchor-placement="bottom-bottom">
-            <img alt="Company Logo" src={job.image} className="job-image" />
-            <div className="job-entire-item-paragraph">
-              <p className="job-title">{job.jobTitle}</p>
-              <p className="job-company-name">{job.companyName}</p>
-              <p className="job-time">{job.time}</p>
-              <div
-                className="job-responsibilities"
-                dangerouslySetInnerHTML={{ __html: job.responsibilities }}
-              />
-              <a className="job-button-link">
-                <button
-                  onClick={() => {
-                    setModalOpen(true);
-                    setJobExperience(index);
-                  }}
-                  className="job-button"
-                >
-                  more details
-                </button>
-              </a>
+          <div key={index.id}>
+            <div
+              className="job-entire-item"
+              key={job.id}
+              data-aos="fade-right"
+              data-aos-anchor-placement="bottom-bottom"
+            >
+              <img alt="Company Logo" src={job.image} className="job-image" />
+              <div className="job-entire-item-paragraph">
+                <p className="job-title">{job.jobTitle}</p>
+                <p className="job-company-name">{job.companyName}</p>
+                <p className="job-time">{job.time}</p>
+                <div
+                  className="job-responsibilities"
+                  dangerouslySetInnerHTML={{ __html: job.responsibilities }}
+                />
+                <a className="job-button-link">
+                  <button
+                    onClick={() => {
+                      setModalOpen(true);
+                      setJobExperience(index);
+                    }}
+                    className="job-button"
+                  >
+                    more details
+                  </button>
+                </a>
+              </div>
             </div>
             {modalOpen && jobExperience === index && (
               <JobExperiencesModal
